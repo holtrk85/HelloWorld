064a9e516b5dcb687962b4c096da285dd8ecb340
KeithHolt <75532311+holtrk85@users.noreply.github.com>
2022-02-03 21:27:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+secret_number = str(random.randint(1,5))
+print("Pick a number between 1 to 5 ")
+while True:
+    res = input("Guess the number: ")
+    if res==secret_number:
+        print("You win")
+        break
+    else:
+        print ("You lose")
+        continue
