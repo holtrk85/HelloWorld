c1ac191da2eb5f599e9b778857028463099c6a2c
holtrk <holtrk85@hotmail.com>
2022-02-03 15:38:00 -0600

    New python project

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+x = ""
+print('Enter your name:')
+x = str(input())
+print('Hello, ' + x )
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  
+for x in range(10):
+  print(x)
